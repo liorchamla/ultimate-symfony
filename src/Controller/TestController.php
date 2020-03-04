68a6153247992d20c694263baa69e3c21a619db6
Lior Chamla <contact@web-develop.me>
2020-03-04 12:16:45 +0100

    Premieres pages

---
+++ src/Controller/TestController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController
+{
+    public function index()
+    {
+        dd("Ca fonctionne");
+    }
+
+    /**
+     * @Route("/test/{age<\d+>?0}", name="test", methods={"GET", "POST"}, host="localhost", schemes={"http", "https"})
+     */
+    public function test(Request $request, $age)
+    {
+        return new Response("Vous avez $age ans !");
+    }
+}
