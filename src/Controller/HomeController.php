8f733ff4b7d2462c38f47fbd87a589bde93edb5c
Lior Chamla <contact@web-develop.me>
2020-03-11 12:27:45 +0100

    Fin de l'intiation et début du projet

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function homepage()
+    {
+        return $this->render('home.html.twig');
+    }
+}
